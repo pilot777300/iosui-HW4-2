1b60139d08009e6d04d363522e00519fab64dc02
pilot777300 <anton225@mail.ru>
2022-07-19 09:39:16 +0300

    set view's corner radius 50

--- Profile/ProfileHeaderView.swift
+++ Profile/ProfileHeaderView.swift
@@ -8,13 +8,65 @@
 import UIKit
 
 class ProfileHeaderView: UIView {
+   
+    var profileView: UIImageView!
+    var label: UILabel!
+        
+      let screenSize = UIScreen.main.bounds
+      
+      override init(frame: CGRect){
+        super.init(frame: frame)
+        profileView = UIImageView(frame: CGRect(x: 15, y: 115, width: 100, height: 100))
+        profileView.backgroundColor = .gray
+        profileView.layer.borderColor = UIColor.white.cgColor
+        profileView.layer.borderWidth = 4.0
+        profileView.layer.cornerRadius = 50
+    
+       // profileView.autoSetDimension(.width, toSize: 124.0)
+       // profileView.autoSetDimension(.height, toSize: 124.0)
+        
+        self.addSubview(profileView)
+          
+          label = UILabel (frame: CGRect(x: 155, y: 155, width: 100, height: 50))
+          label.backgroundColor = .white
+          self.addSubview(label)
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+//-------------------
+//    let label = UILabel(frame: CGRect(x: 150, y: 350, width: 100, height: 20))
+//    let picture = UIImageView()
+//    let button = UIButton(frame: CGRect(x: 100, y: 300, width: 200, height: 50))
+//
+//    func updateText(_ text:String?) {
+//           guard let text = text else { return }
+//           label.text = text
+//       }
+//
+//       override init(frame: CGRect){
+//           super.init(frame: frame)
+//           label.translatesAutoresizingMaskIntoConstraints = false
+//           label.text = "OneView!"
+//           label.numberOfLines = 0
+//           label.textColor = .red
+//           label.textAlignment = .center
+//           button.backgroundColor = .blue
+//           button.layer.cornerRadius = 12
+//           button.setTitle("Push", for: .normal)
+//           self.addSubview(label)
+//           self.addSubview(button)
+//       }
+//
+//       override func layoutSubviews() {
+//           super.layoutSubviews()
+//           label.frame = bounds
+//           button.frame = bounds // не было вначале
+//       }
+//
+//       required init?(coder aDecoder: NSCoder) {
+//           fatalError("init(coder:) has not been implemented")
+//       }
+}
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
-
 }
--- Profile/ProfileViewController.swift
+++ Profile/ProfileViewController.swift
@@ -8,11 +8,63 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+   
+    var profile: ProfileHeaderView!
+    // let myview = ProfileHeaderView()
+     //let parview = ProfileHeaderView()
 
+//     override func loadView() {
+//         let view = UIView()
+//         view.backgroundColor = .black
+//         self.view = view
+//     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemMint
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .lightGray
+        profile = ProfileHeaderView(frame: CGRect.zero)
+          self.view.addSubview(profile)
+ //------------------------
+//        setupLabels()
+//        view.addSubview(aview)
+//
+//        // setupStack()
+//
+//            }
+//
+//            func setupLabels() {
+//                aview.updateText("A View")
+//                aview.label.textColor = .black
+//                aview.label.font.withSize(14)
+//
+//                myview.updateText("Another View with more to say - this is a full bit of text")
+//               parview.updateText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis porttitor interdum ex dictum auctor. Proin nec imperdiet sem. Duis pulvinar tortor viverra elit convallis tempus. Maecenas egestas elit eu convallis mattis. Sed non hendrerit tortor. Phasellus lectus dolor, pretium sed dignissim scelerisque, vulputate id dolor. Sed ac rhoncus enim.")
+//                // can also access the label directly - but better to make a function if this will be changed often.
+//                myview.label.textColor = .white
+ //           }
+       
+//
+//            func setupStack() {
+//                let subviews = aview
+               // let stackView = UIStackView()
+//                for view in subviews {
+//                    stackView.addArrangedSubview(view)
+  //              }
+//                stackView.axis = .vertical
+//                stackView.distribution = .fillEqually
+//                stackView.alignment = .fill
+//                stackView.spacing = 10
+//                stackView.translatesAutoresizingMaskIntoConstraints = false
+
+//                view.addSubview(stackView)
+//                //autolayout the stack view
+//                let sH = NSLayoutConstraint.constraints(withVisualFormat: "H:|-20-[stackView]-20-|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: ["stackView":stackView])
+//                let sV = NSLayoutConstraint.constraints(withVisualFormat: "V:|-30-[stackView]-30-|", options: NSLayoutConstraint.FormatOptions(rawValue:0), metrics: nil, views: ["stackView":stackView])
+//                view.addConstraints(sH)
+//                view.addConstraints(sV)
+//            }
+
     }
+
+
 
 }
